0131aff52f5d571013e53dcf714d72de2bd04dc9
tetete2016 <tetetelego2013@gmail.com>
2016-05-14 20:15:48 +0900

    added high score submit button but highscore does not work now

--- index.js
+++ index.js
@@ -3,20 +3,24 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-//app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 app.use(express.static('public'));
-
-// views is directory for all template files
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
     //response.render('pages/index');
     response.send("error");
 });
 
+app.post('/highscore', function (request, response) {
+    //console.log(request);
+    console.log(request.body);
+    response.send("score sent!!");
+});
+
+app.get('/highscore', function (request, response) {
+    console.log(request.body);
+    response.send("score");
+});
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
