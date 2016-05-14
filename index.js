eb5fb03a3020f39c03727c535e20cd357661a071
tetete2016 <tetetelego2013@gmail.com>
2016-05-15 08:55:11 +0900

    changed

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
-//var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(bodyParser.json());
 
 app.use(express.static('public'));
