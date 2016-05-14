48e569ed1e4fff4b14ad896c3854e1863443e6d2
tetete2016 <tetetelego2013@gmail.com>
2016-05-14 20:19:16 +0900

    changed

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(bodyParser.json());
+
 app.use(express.static('public'));
 
 app.get('/', function(request, response) {
-    //response.render('pages/index');
     response.send("error");
 });
-
+/*
 app.post('/highscore', function (request, response) {
-    //console.log(request);
     console.log(request.body);
     response.send("score sent!!");
 });
@@ -21,6 +20,7 @@ app.get('/highscore', function (request, response) {
     console.log(request.body);
     response.send("score");
 });
+*/
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
