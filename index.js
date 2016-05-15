7fbee73769be868550740d77980e00202b89e7d8
tetete2016 <tetetelego2013@gmail.com>
2016-05-15 21:19:20 +0900

    add backdoor

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.listen(app.get('port'), function () {
 function getdata() {
     console.log("getdata()");
     pg.connect(process.env.DATABASE_URL, function (err, client, done) {
-        client.query('SELECT * FROM highscore', function (err, result) {
+        client.query('SELECT * FROM highscore;', function (err, result) {
             done();
             if (err)
             { console.error(err); return null; }
