440393c7f7a44697a191ac4d88c4ac6ce1ef679e
tetete2016 <tetetelego2013@gmail.com>
2016-05-15 11:35:49 +0900

    database test

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.listen(app.get('port'), function () {
 
 app.get('/db', function (request, response) {
     pg.connect(process.env.DATABASE_URL, function (err, client, done) {
-        client.query('SELECT * FROM test_table', function (err, result) {
+        client.query('SELECT * FROM highscore', function (err, result) {
             done();
             if (err)
             { console.error(err); response.send("Error " + err); }
