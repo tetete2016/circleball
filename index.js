513b846916333753431dc0046c0539dd4b76b329
tetete2016 <tetetelego2013@gmail.com>
2016-05-15 11:23:53 +0900

    database test

--- index.js
+++ index.js
@@ -65,7 +65,9 @@ app.get('/db', function (request, response) {
             if (err)
             { console.error(err); response.send("Error " + err); }
             else
-            { response.render('pages/db', { results: result.rows }); }
+            {
+                response.send(JSON.stringify(result.rows));
+            }
         });
     });
 })
